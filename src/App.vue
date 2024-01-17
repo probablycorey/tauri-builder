7ed67faeab24a5a4650626e16a1a8f75e82da185
Corey Johnson <probablycorey@gmail.com>
2024-01-16 16:11:51 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { convertFileSrc } from "@tauri-apps/api/tauri"
+import { onMounted, ref } from "vue"
+const output = ref<HTMLPreElement>()
+
+onMounted(async () => {
+  if (!output.value) throw new Error("output element not found")
+
+  const assetUrl = convertFileSrc("/Users/corey/Desktop/thing-to-build/dist/thing-to-build.js")
+  const theThing = (await import(assetUrl)).default
+  console.log(`🌈 `, theThing)
+  theThing(output.value)
+})
+</script>
+
+<template>
+  <div>Hi corey</div>
+  <pre ref="output"></pre>
+</template>
+
+<style scoped></style>
